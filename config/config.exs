96d6f707fd63cf3689cc04607fe2ad98c0ff1279
Todd Anderson <todd@fins.surf>
2024-03-15 17:42:50 -0700

    cleaned it up a bit

--- config/config.exs
+++ config/config.exs
@@ -26,11 +26,11 @@ config :kujibot, KujibotWeb.Endpoint,
 # for making outbound HTTP requests from the server.
 config :tesla, adapter: Tesla.Adapter.Hackney
 
-# Oban base configuration, keep completed, cancelled or discarded jobs for 12 hours
+# Oban base configuration, keep completed, cancelled or discarded jobs for 1 hour
 config :kujibot, Oban,
   repo: Kujibot.Repo,
-  queues: [default: 10, wallets: 20],
-  plugins: [{Oban.Plugins.Pruner, max_age: 43_200}]
+  queues: [default: 10, wallet: 10],
+  plugins: [{Oban.Plugins.Pruner, max_age: 3_600}]
 
 # Configures the mailer
 #
--- lib/kujibot/jobs/create_wallet.ex
+++ lib/kujibot/jobs/create_wallet.ex
@@ -1,15 +1,13 @@
 defmodule Kujibot.Jobs.CreateWallet do
   use Oban.Worker,
-    queue: "wallet",
+    queue: :wallet,
     unique: [
-      period: 60,
-      fields: [:args],
-      states: [:available, :scheduled, :executing]
+      keys: [:chat_id],
+      period: 60
     ]
 
   @impl true
-  def perform(%{"chat_id" => chat_id} = _args) do
-    # Perform your task here. This is where you check if the expected info is present and then proceed.
-    # If the task is completed, this function should return `:ok` or a similar success indicator.
+  def perform(%Oban.Job{args: %{"chat_id" => chat_id}}) do
+    # Your job logic here
   end
 end
